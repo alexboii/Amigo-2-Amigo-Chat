f8f3e880431b782e5340f8d9699af8c5a4b6a248
alexboii <potteref@gmail.com>
2017-11-11 18:48:20 -0500

    init

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../assets/css/App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Chat boilerplate for our amazing upcoming app</h1>
+        <p>Here's where we gonna start building shit!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
