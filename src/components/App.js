878f08d460f815fb5b5d080c90bee7babdd1f4d1
alexboii <potteref@gmail.com>
2017-11-14 00:10:10 -0500

    first interface added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import '../assets/css/App.css';
-import React, { Component } from 'react';
+import filippo_image from "../assets/images/filippo_image.jpg";
+import React, { Component, Button } from "react";
+import "../assets/css/App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Chat boilerplate for our amazing upcoming app</h1>
-        <p>Here's where we gonna start building shit!</p>
+      <div class="main">
+        <div class="header">
+          <h1>Philippo-2-Philippo Chat</h1>
+          <img class="img-circle" src={filippo_image} />
+          <h4>
+            A peer-to-peer chat client featuring NAT traversal and encryption.
+          </h4>
+        </div>
+        <div class="server-address-holder">
+          <input type="text" placeholder="Server Address & Port" />
+        </div>
+        <div class="button-holder">
+          <button class="button button--nuka button--round-s button--text-thick">
+            Create Session
+          </button>
+          <button class="button button--nuka button--round-s button--text-thick">
+            Join Session
+          </button>
+        </div>
       </div>
     );
   }
