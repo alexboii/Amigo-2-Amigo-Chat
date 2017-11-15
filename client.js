cda8d42431ef7e5bca711b294c2109c309ae89f2
Kartoshka <eli.harfouche@gmail.com>
2017-11-15 11:52:54 -0500

    Get tiemout working between client-client and client-server

--- client.js
+++ client.js
@@ -2,28 +2,38 @@
 const dgram = require('dgram');
 //TCP
 const net = require('net');
+//Command line input
 const readline = require('readline');
 
-const SERVER_ADDRESS = "13.82.236.21";
+//Address of matchmaking/STUN server
+const SERVER_ADDRESS = "142.157.24.139";
 const SERVER_PORT = 80;
 
-
+//UDP socket used for communicating with server and keeping router hole open
 var udp_socket = dgram.createSocket('udp4');
 
-const message = Buffer.from('Some bytes');
-const client = dgram.createSocket('udp4');
-
+//TEMPORARY READLINE INTERFACE FOR MESSAGIN
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+//Used for keep alive function
+let rng_id = 0;
+let timeoutHandle;
+let ackd = false;
+let serverAck = false;
+
+//Information retrieved from STUN part of server to know what our address is
+var self_address;
+var self_port;
+
+//Information retrieved from matchmaking part of server to know what client we're talking to
 var peer_address;
 var peer_port;
-var ACK = false;
 
 udp_socket.on('error', (err) => {
-    console.log(`server error:\n${err.stack}`);
+    console.log(`client error:\n${err.stack}`);
     udp_socket.close();
   });
   
@@ -36,13 +46,25 @@ udp_socket.on('message', (msg, rinfo) => {
     }
     switch(msg.type)
     {
-        case 'ACK':
-            //Do something to update that we've acknowledged this message
-            //TODO update to support three way handshaking
+        case 'ACK':        
             if(msg.peer_ip)
             {
-            connectToPeer(msg.peer_ip,msg.peer_port); 
+                //Message from server 
+                serverAck = true;
+                connectToPeer(msg.peer_ip,msg.peer_port); 
             }
+            //If we receive an ACK from the other client, then we know we've successfully kept the UDP NAT hole punch open
+            else if(msg.source_ip != SERVER_ADDRESS || msg.source_port != SERVER_PORT)
+            {
+                ackd = NotifyAck(msg.id);
+                //Make keep alive function go back to sleep
+            }
+            else
+            {
+                //ACK from server
+                serverAck = true;
+            }
+
         break;
         case 'CON':
             connectToPeer(msg.peer_ip,msg.peer_port);
@@ -51,24 +73,75 @@ udp_socket.on('message', (msg, rinfo) => {
             console.log(`\nReceived: ${msg.msg}`);
             msgUpdate();        
         break;
+        case 'SYN':
+            //Acknowledge the keep alive signal
+            var ackSYN = {type:'ACK',source_ip:self_address,source_port:self_port, id:(msg.id + 1)};
+            udp_socket.send(Buffer.from(JSON.stringify(ackSYN)),peer_port,peer_address)
+        break;
     }
   });
-  
+let serverTimeout;
 udp_socket.on('listening', () => {
-    const address = udp_socket.address();
-    var msg = {type:"REQ"};
-    udp_socket.send(Buffer.from(JSON.stringify(msg)),SERVER_PORT,SERVER_ADDRESS);
-    //console.log(`server listening ${address.address}:${address.port}`);
-  });
+    rl.question("Please input a matchmaking key: ", (answer) => {
+        var msg = {type:'REQ',msg:answer};
+        let attempts = 1;
+        udp_socket.send(Buffer.from(JSON.stringify(msg)),SERVER_PORT,SERVER_ADDRESS);        
+        serverTimeout = setInterval(() => {
+            if(attempts > 3)
+            {
+                console.log("Could not contact server");
+                process.exit();
+            }
+            else if(!serverAck)
+            {
+                console.log(`Attempting to contact server (Attempt ${attempts})`)
+                var syn_msg = {type:'SYN', id:rng_id}   
+                udp_socket.send(Buffer.from(JSON.stringify(msg)),SERVER_PORT,SERVER_ADDRESS);
+            }
+            else
+            {
+                console.log("server responded???");
+                serverAck = false;
+                clearInterval(serverTimeout);
+            }
+            attempts++;
+        }, 3000);
+    });
+});
 
 udp_socket.bind();
 
 var connectToPeer = function(address, port)
 {
-    console.log(`Connected to peer ${address}:${port}\n`);
+    console.log(`Connecting to peer ${address}:${port}\n`);
+    KeepAlive();
+    
     peer_address = address;
     peer_port = port;
-    msgUpdate();
+
+    var attempts =0;
+    ackd = false;
+    rng_id = Math.floor(Math.random()*1000);    
+    
+    let initConnect = setInterval(() => {
+        if(++attempts > 3)
+        {
+            console.log("Could not connect to peer");
+            process.exit();
+        }
+        else if(!ackd)
+        {
+            var syn_msg = {type:'SYN', id:rng_id}   
+            udp_socket.send(Buffer.from(JSON.stringify(syn_msg)),peer_port,peer_address);            
+        }
+        else
+        {
+            clearInterval(initConnect);
+            //Once we've established a reliable UDP connection, we can start messaging
+            msgUpdate();
+        }
+        
+    }, 3000);
 }
 
 var msgUpdate =function()
@@ -81,5 +154,32 @@ var msgUpdate =function()
     });
 }
 
+function KeepAlive() {
+    if(timeoutHandle)
+    {
+        clearInterval(timeoutHandle);
+    }
+    rng_id = Math.floor(Math.random()*1000);  
+    var syn_msg = {type:'SYN', id:rng_id}    
+    timeoutHandle = setInterval(() => {
+        udp_socket.send(Buffer.from(JSON.stringify(syn_msg)),peer_port,peer_address);
+    }, 3000);
+}
 
+function NotifyAck(id) {
 
+    if(id != (rng_id +1))
+    {
+        return false;;
+    }
+
+    //This stops a thing from repeating
+    if (timeoutHandle) {
+        clearInterval(timeoutHandle);
+    }
+
+    timeoutHandle = setInterval(() => {
+        KeepAlive(); 
+    }, 15000);
+    return true;
+}
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dgram = require('dgram');
 //TCP
 const net = require('net');
 
-var SERVER_ADDRESS = "13.82.236.21";
+var SERVER_ADDRESS = "142.157.24.139";
 var SERVER_PORT = 80;
 
 var udp_socket = dgram.createSocket('udp4');
@@ -11,7 +11,7 @@ var udp_socket = dgram.createSocket('udp4');
 var saved_ip = "";
 var saved_port = 0;
 
-//var connections = {}
+var connections = {}
 
 udp_socket.on('error', (err) => {
     console.log(`server error:\n${err.stack}`);
@@ -30,44 +30,72 @@ udp_socket.on('message', (msg, rinfo) => {
         case "REQ":
             console.log(`Receive request from ${rinfo.address}:${rinfo.port} \n`);
             //Request for another peer, we acknowledge it and only respond if we can match two peers
-
-            if(saved_ip == "")
+            console.log(`msg is: ${msg.msg}`);
+            if(msg.msg in connections)
             {
-                //TODO Potentially a triple handshake ACK, where we only save the info when we receive the ACK back in the server
-                // In that case we wouldn't embed anything in an ACK message, we'd send a separate CON message to each
-                // This wouldn't be exactly how TCP does its 3-way handshake but whatever
-                
-                //Acknowledge peer, but we have no one to connect them with
-                var ack_msg = {type:'ACK', self_ip:rinfo.address, self_port: rinfo.port};
-                var msgBuffer = Buffer.from(JSON.stringify(ack_msg));
-                udp_socket.send(msgBuffer,rinfo.port,rinfo.address);
-
-                //Save information to pass onto next peer that connects
-                saved_port = rinfo.port;
-                saved_ip =rinfo.address;
-            }
-            //If the same peer connects again, let's not connect them to themselves
-            //The 
-            else if(saved_ip != rinfo.address || saved_port != rinfo.port)
-            {
-                console.log(`Attempting to establish connection between ${saved_ip}:${saved_port} and ${rinfo.address}:${rinfo.port} \n`);
-                //TODO: since this is udp, we have no way of ensuring messages arrive, so a more robust handshake would solve it.
-                //      original python solution was to timeout a client if they haven't received a connection
-                //      we can use some sort of timeout system where we just cancel shit if we dont hear back
+                console.log(`Attempting to establish connection between ${connections[msg.msg].address}:${connections[msg.msg].port} and ${rinfo.address}:${rinfo.port} \n`);
 
                 //Acknowledge newly connected peer, passing connection info through with ACK
-                var ack_msg1 = {type:'ACK', self_ip : rinfo.address, self_port : rinfo.port, peer_ip : saved_ip, peer_port : saved_port};
+                var ack_msg1 = {type:'ACK', self_ip : rinfo.address, self_port : rinfo.port, peer_ip : connections[msg.msg].address, peer_port : connections[msg.msg].port};
                 //Inform second peer that we have a connection ready for it
-                var con_msg = {type :'CON', self_ip : saved_ip, self_port : saved_port, peer_ip : rinfo.address, peer_port : rinfo.port};
+                var con_msg = {type :'CON', self_ip : connections[msg.msg].address, self_port : connections[msg.msg].port, peer_ip : rinfo.address, peer_port : rinfo.port};
 
                 //Messaged each peer
                 udp_socket.send( Buffer.from(JSON.stringify(ack_msg1)),rinfo.port,rinfo.address);
-                udp_socket.send( Buffer.from(JSON.stringify(con_msg)), saved_port, saved_ip);
+                udp_socket.send( Buffer.from(JSON.stringify(con_msg)), connections[msg.msg].port, connections[msg.msg].address);
 
-                //Reset flags
-                saved_ip = "";
-                saved_port = 0;
+                //Delete entry
+                delete connections[msg.msg];
+ 
             }
+            else
+            {
+                //Save connection parameter
+                connections[msg.msg] = {address:rinfo.address,port:rinfo.port};
+
+                //Acknowledge peer, but we have no one to connect them with
+                var ack_msg = {type:'ACK', source_ip: SERVER_ADDRESS, source_port: SERVER_PORT, self_ip:rinfo.address, self_port: rinfo.port};
+                var msgBuffer = Buffer.from(JSON.stringify(ack_msg));
+                udp_socket.send(msgBuffer,rinfo.port,rinfo.address);
+            }
+
+            // if(saved_ip == "")
+            // {
+            //     //TODO Potentially a triple handshake ACK, where we only save the info when we receive the ACK back in the server
+            //     // In that case we wouldn't embed anything in an ACK message, we'd send a separate CON message to each
+            //     // This wouldn't be exactly how TCP does its 3-way handshake but whatever
+                
+            //     //Acknowledge peer, but we have no one to connect them with
+            //     var ack_msg = {type:'ACK', self_ip:rinfo.address, self_port: rinfo.port};
+            //     var msgBuffer = Buffer.from(JSON.stringify(ack_msg));
+            //     udp_socket.send(msgBuffer,rinfo.port,rinfo.address);
+
+            //     //Save information to pass onto next peer that connects
+            //     saved_port = rinfo.port;
+            //     saved_ip =rinfo.address;
+            // }
+            // //If the same peer connects again, let's not connect them to themselves
+            // //The 
+            // else if(saved_ip != rinfo.address || saved_port != rinfo.port)
+            // {
+            //     console.log(`Attempting to establish connection between ${saved_ip}:${saved_port} and ${rinfo.address}:${rinfo.port} \n`);
+            //     //TODO: since this is udp, we have no way of ensuring messages arrive, so a more robust handshake would solve it.
+            //     //      original python solution was to timeout a client if they haven't received a connection
+            //     //      we can use some sort of timeout system where we just cancel shit if we dont hear back
+
+            //     //Acknowledge newly connected peer, passing connection info through with ACK
+            //     var ack_msg1 = {type:'ACK', self_ip : rinfo.address, self_port : rinfo.port, peer_ip : saved_ip, peer_port : saved_port};
+            //     //Inform second peer that we have a connection ready for it
+            //     var con_msg = {type :'CON', self_ip : saved_ip, self_port : saved_port, peer_ip : rinfo.address, peer_port : rinfo.port};
+
+            //     //Messaged each peer
+            //     udp_socket.send( Buffer.from(JSON.stringify(ack_msg1)),rinfo.port,rinfo.address);
+            //     udp_socket.send( Buffer.from(JSON.stringify(con_msg)), saved_port, saved_ip);
+
+            //     //Reset flags
+            //     saved_ip = "";
+            //     saved_port = 0;
+            // }
 
             break;
             case "ACK":
